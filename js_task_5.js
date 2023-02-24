cb911d08e09c10e7daaa13db9065821f9e942185
HarshanAruchami <harshanaruchami@gmail.com>
2023-02-24 17:02:29 +0530

    final commit

--- js_task_5.js
+++ js_task_5.js
@@ -1,14 +1,34 @@
-function pdform(){
+function form() {
+    var name = document.register.uname.value;
+    var pass = document.register.pass.value;
+    var mail = document.register.mail.value;
+    var gen = document.register.gen.value;
+    var add = document.register.add.value;
+    var date = document.register.date.value;
+    var mob = document.register.mob.value;
+    var tele = document.register.tel.value;
+    var pro = document.register.pro.value;
+    var cBox = document.getElementById('repass');
 
-var name = document.getElementById("uname").value;
-var pass = document.getElementById(pass).value;
-var mail = document.getElementById(mail).value;
-var gen = document.getElementById(gen).value;
-var add = document.getElementById(add).value;
-var repass = document.getElementById(repass).value;
-var date = document.getElementById(date).value;
-var pro = document.getElementById(pro).value;
-var mob = document.getElementById(mob).value;
-var tel = document.getElementById(tel).value;
-document.writeln("hlooo"+ name);
+    document.write('Name:' + " " + name + "<br>");
+    document.write('Password: ' + " " + pass + "<br>");
+    document.write('E-mail:' + " " + mail + "<br>");
+    document.write('Gender: ' + " " + gen + "<br>");
+    document.write('Address: ' + " " + add + "<br>");
+    document.write('Remember password: ' + " " );
+    if (cBox.checked==true) {
+        document.write("yes" + "<br>");
+    }
+    else {
+        document.write("No" + "<br>");
+    }
+    document.write('Date of Birth: ' + "" + date + "<br>");
+    document.write('Mobile Number : ' + " " + mob + "<br>");
+    document.write('Tel No : ' + " " + tele + "<br>");
+    document.write('Profession : ' + " " + pro + "<br>");
+
+
+    
+
+
 }
