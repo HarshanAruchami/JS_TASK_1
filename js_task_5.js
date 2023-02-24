46b0dcf76aef47ff308af80f3acd3f63d6f526b6
HarshanAruchami <harshanaruchami@gmail.com>
2023-02-24 06:57:19 +0530

    display details 1st commit

---
+++ js_task_5.js
@@ -0,0 +1,14 @@
+function pdform(){
+
+var name = document.getElementById("uname").value;
+var pass = document.getElementById(pass).value;
+var mail = document.getElementById(mail).value;
+var gen = document.getElementById(gen).value;
+var add = document.getElementById(add).value;
+var repass = document.getElementById(repass).value;
+var date = document.getElementById(date).value;
+var pro = document.getElementById(pro).value;
+var mob = document.getElementById(mob).value;
+var tel = document.getElementById(tel).value;
+document.writeln("hlooo"+ name);
+}
